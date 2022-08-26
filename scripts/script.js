32028fd866173ea186f2337096857f09d1f04219
Erick Castilho Mestre <erickcastilhomestre@gmail.com>
2022-08-26 19:00:36 -0300

    correção de classes e adição de bootstrap-icons

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+const activeTextInput = () => {
+    if(document.getElementById("flexRadioDefault3").checked){
+        document.getElementById("otherRadioCheck").disabled = false;
+    }         
+}
